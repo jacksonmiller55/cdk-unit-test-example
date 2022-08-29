93761796439c112b85ff6024cfb85e06c4f1b4c5
Jackson Miller <jacksonmiller55@gmail.com>
2022-08-28 19:55:27 -0500

    update test for query

--- sql_alchemy_lambda/tests/sql_alchemy_lambda/test_lambda_handler.py
+++ sql_alchemy_lambda/tests/sql_alchemy_lambda/test_lambda_handler.py
@@ -1,7 +1,7 @@
 import json
 from unittest.mock import patch
 from sql_alchemy_lambda.dbmodels import Book
-from sql_alchemy_lambda.lambda_handler import get_book
+from sql_alchemy_lambda.lambda_handler import get_book, build_book_query
 
 
 @patch('sql_alchemy_lambda.lambda_handler.build_book_query')
@@ -46,3 +46,37 @@ def test_get_book(mock_session, mock_validate_get_book_params, mock_build_book_q
         assert res["publisher"] == exp["publisher"]
         assert res["title"] == exp["title"]
         assert res["year"] == exp["year"]
+
+
+@patch('sql_alchemy_lambda.lambda_handler.Session')
+def test_build_book_query(mock_db_session):
+    validated_params = {
+        "id": 1,
+        "title": "The Title",
+        "author": "Joe Smith",
+        "publisher": "Bobby Jones",
+        "year": 1965
+    }
+
+    not_used = build_book_query(mock_db_session, validated_params)
+    mock_db_session.query.assert_called_once_with(Book)
+    mock_db_session.query().filter.assert_called_once()
+    assert mock_db_session.query().method_calls[0][0] == 'filter'
+    assert mock_db_session.query().method_calls[0][1][0].__str__() == 'book.id = :id_1'
+
+    mock_db_session.query().filter().filter.assert_called_once()
+    assert mock_db_session.query().filter().method_calls[0][0] == 'filter'
+    assert mock_db_session.query().filter().method_calls[0][1][0].__str__() == 'book.title = :title_1'
+
+    mock_db_session.query().filter().filter().filter.assert_called_once()
+    assert mock_db_session.query().filter().filter().method_calls[0][0] == 'filter'
+    assert mock_db_session.query().filter().filter().method_calls[0][1][0].__str__() == 'book.author = :author_1'
+
+    mock_db_session.query().filter().filter().filter().filter.assert_called_once()
+    assert mock_db_session.query().filter().filter().filter().method_calls[0][0] == 'filter'
+    assert mock_db_session.query().filter().filter().filter().method_calls[0][1][0].__str__() == 'book.publisher = :publisher_1'
+
+    mock_db_session.query().filter().filter().filter().filter().filter.assert_called_once()
+    assert mock_db_session.query().filter().filter().filter().filter().method_calls[0][0] == 'filter'
+    assert mock_db_session.query().filter().filter().filter().filter().method_calls[0][1][0].__str__() == 'book.year = :year_1'
+
